32517f58dc47535b2c6ef53fcc1d74ba70f3660c
Jeffery Wilson <jeff@jeffalwilson.com>
2016-04-30 14:36:36 -0400

    Update steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -660,7 +660,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {160.58, 160.58, 8147.943, 1621.943}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {159.86, 159.62, 7999.944, 1621.943}
 #define DEFAULT_MAX_FEEDRATE          {300, 300, 2, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000, 3000, 75, 10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
